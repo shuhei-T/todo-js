50ed318c721e59a7aa49de4716d644d862c35195
shuhei-T <81573592+shuhei-T@users.noreply.github.com>
2022-06-09 15:48:37 +0900

    タスクの戻す機能

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteList(inputText);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // liタグ生成
   const li = document.createElement("li");
 
@@ -14,7 +24,7 @@ const onClickAdd = () => {
 
   // pタグ生成
   const p = document.createElement("p");
-  p.innerText = inputText;
+  p.innerText = text;
 
   // button(完了)タグ生成
   const completeButton = document.createElement("button");
@@ -24,13 +34,17 @@ const onClickAdd = () => {
     deleteFromIncompleteList(completeButton.parentNode.parentNode);
 
     // 完了リストに追加する要素
-    const addTarget = completeButton.parentNode;
+    const addTarget = completeButton.parentNode.parentNode;
 
     // TODO内容テキストを取得
-    const text = addTarget.firstElementChild.innerText;
+    const text = addTarget.firstChild.firstElementChild.innerText;
 
     // div以下を初期化
     addTarget.textContent = null;
+
+    // divタグ生成
+    const div = document.createElement("div");
+    div.className = "list-row";
 
     // pタグ生成
     const p = document.createElement("p");
@@ -39,10 +53,20 @@ const onClickAdd = () => {
     // buttonタグ生成
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグ(li)を完了リストから削除
+      const deleteTarget = backButton.parentNode.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // テキスト取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
 
     // divタグの子要素に各要素を設定
-    addTarget.appendChild(p);
-    addTarget.appendChild(backButton);
+    addTarget.appendChild(div);
+    div.appendChild(p);
+    div.appendChild(backButton);
 
     // 完了リストに追加
     document.getElementById("complete-list").appendChild(addTarget);
@@ -66,11 +90,6 @@ const onClickAdd = () => {
 
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(li);
-};
-
-// 未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
